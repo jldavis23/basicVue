0a654dd4121ab4a17cf74db92438cf6a5fc03b81
Jacey Davis <jaceyd923@gmail.com>
2023-02-22 17:39:45 -0700

    fetch data from pokemon api

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
+let id = 0
+let pokeArray;
+
 createApp({
     data() {
         return {
-            message: "hello world"
+            pokemon: [],
+            currentStats: null
         }
+    },
+    methods: {
+        getPokemon() {
+            for (let i = 1; i <= pokeArray.length; i++) {
+                fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.pokemon = [...this.pokemon, data]
+                })
+            }
+            
+        },
+        showStats(poke) {
+            console.log(poke)
+            this.currentStats = poke
+        }
+    },
+    mounted() {
+        fetch('https://pokeapi.co/api/v2/pokemon')
+                .then(response => response.json())
+                .then(data => {
+                    pokeArray = data.results
+                    this.getPokemon()
+                })
     }
 }).mount('#app')
